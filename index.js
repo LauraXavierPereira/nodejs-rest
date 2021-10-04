ec05965b9dab98cef1bc979a481c8a21dcf5680a
lauraxavierr <laurausual@gmail.com>
2021-10-04 20:48:41 -0300

    feat: realizando alteração de credenciais do banco e tratativa do campo de data.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const customExpress = require('./config/customExpress')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
-conexao.connect(() => {
+conexao.connect(error => {
 	if(error){
 		console.log(error)
 	} else {
