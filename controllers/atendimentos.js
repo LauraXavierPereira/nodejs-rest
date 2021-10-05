1150f781f99936b4103531c4c4037a97be0f30ce
lauraxavierr <laurausual@gmail.com>
2021-10-04 21:39:25 -0300

    feat: criando listagem de produtos total e por ID.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,13 +1,21 @@
-const Atendimento = require('../models/atendimentos')
+const Atendimentos = require('../models/atendimentos')
 
 module.exports = app => {
-	app.get('/atendimentos', (req, res) => res.send('Rota de atendimentos'))
+	app.get('/atendimentos', (req, res) => {
+		Atendimentos.lista(res)
+	})
+
+	app.get('/atendimentos/:id', (req, res) => {
+		const id = parseInt(req.params.id) //Convertendo ID para inteiro
+
+		Atendimentos.buscaPorId(id, res)
+	})
 
 	app.post('/atendimentos', (req, res) => {
 		console.log(req.body)
 		
-		const atendimento = req.body
-		Atendimento.adiciona(atendimento, res)
+		const atendimentos = req.body
+		Atendimentos.adiciona(atendimentos, res)
 
 		//res.send('Enviando dados de atendimentos')
 	})
