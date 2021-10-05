4b59952b0e4caa80662bd9fc5e28efec44c65119
lauraxavierr <laurausual@gmail.com>
2021-10-04 21:23:45 -0300

    feat: realizando tratamento de erros de acordo com a regra de negócio.

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,24 +1,48 @@
-//Conexoes com o banco de dados
 const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-	adiciona(atendimento, res) {
-		const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-		const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-		const atendimentoDatado = {...atendimento, dataCriacao, data} //Tudo de atendimento + Data
-		const sql = 'INSERT INTO atendimentos SET ?'
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
+            }
+        ]
 
-		conexao.query(sql, atendimentoDatado, (error, results) => {
-			if (error){
-				res.status(400).json(error)
-			} else {
-				console.log("SUCCESS")
-				res.status(201).json(results)
-			}
-		})
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
 
-	}
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            const atendimentoDatado = {...atendimento, dataCriacao, data} //Tudo de atendimento + Data
+
+            const sql = 'INSERT INTO Atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+					console.log("SUCCESS")
+                    res.status(201).json(atendimento)
+                }
+            })
+        }
+       
+    }
 }
+
 module.exports = new Atendimento
