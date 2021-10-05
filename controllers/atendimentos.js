451ccb6f0060654a4fb0d670eabfbd371ebca3fc
lauraxavierr <laurausual@gmail.com>
2021-10-04 21:55:38 -0300

    feat: criando método patch para atualização de atendimento.

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,22 +1,29 @@
-const Atendimentos = require('../models/atendimentos')
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
 	app.get('/atendimentos', (req, res) => {
-		Atendimentos.lista(res)
+		Atendimento.lista(res)
 	})
 
 	app.get('/atendimentos/:id', (req, res) => {
 		const id = parseInt(req.params.id) //Convertendo ID para inteiro
 
-		Atendimentos.buscaPorId(id, res)
+		Atendimento.buscaPorId(id, res)
 	})
 
 	app.post('/atendimentos', (req, res) => {
 		console.log(req.body)
 		
 		const atendimentos = req.body
-		Atendimentos.adiciona(atendimentos, res)
+		Atendimento.adiciona(atendimentos, res)
 
 		//res.send('Enviando dados de atendimentos')
+	})
+
+	app.patch('/atendimentos/:id', (req, res) => {
+		const id = parseInt(req.params.id) //Convertendo ID para inteiro
+		const valores = req.body
+
+		Atendimento.altera(id, valores, res)
 	})
 }
